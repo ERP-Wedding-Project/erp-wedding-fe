4ae3b36eaaf27ef33fa829075ebdd55f91625489
Handie-PC <hanzdeveloper@gmail.com>
2026-02-28 21:37:57 +0700

    feat: auth

---
+++ src/stores/auth.ts
@@ -0,0 +1,8 @@
+import {defineStore} from "pinia";
+import ApiService from "@/core/services/ApiService";
+import router from "@/router";
+
+
+export const useAuthStore = defineStore("auth", () => {
+
+})
