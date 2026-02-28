e48233d2540d04f77d4ff7fcb977e6b22a3c3284
Handie-PC <hanzdeveloper@gmail.com>
2026-02-28 21:37:08 +0700

    feat: init api service and toastify integration

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./assets/css/app.css";
+import ApiService from "@/core/services/ApiService";
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
 
 const app = createApp(App);
 const pinia = createPinia()
@@ -10,5 +12,9 @@ const pinia = createPinia()
 // app.use(createPinia());
 app.use(pinia)
 app.use(router);
+app.use(Vue3Toastify, {
+    autoClose: 3000,
+} as ToastContainerOptions);
+ApiService.init(app);
 
 app.mount("#app");
